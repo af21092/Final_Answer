333f338f5f57e22235c073c73fd80325ddee4157
Fumika Matsuo <af21092@shibaura-it.ac.jp>
2025-03-27 10:42:59 +0900

    1-2.py create

---
+++ python/ex1_web-scraping/1-2.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re   
+import time
+import ssl
+from urllib.parse import urlparse
+import pandas as pd
+from urllib.parse import urljoin
