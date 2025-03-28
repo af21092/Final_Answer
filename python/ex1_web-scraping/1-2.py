b3601e7e2d0f3113bb20aa838881e3f7c5e10e8d
Fumika Matsuo <af21092@shibaura-it.ac.jp>
2025-03-28 11:54:23 +0900

    1-2完成

--- python/ex1_web-scraping/1-2.py
+++ python/ex1_web-scraping/1-2.py
@@ -16,21 +16,15 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
+
+#ユーザーエージェントの設定
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
 options.add_argument(f'user-agent={user_agent}')
 pf_path = r"C:\Users\fu3ka\AppData\Local\Google\Chrome\User Data\Default"
 options.add_argument(f'user-data-dir={pf_path}')
+
 driver = webdriver.Chrome(options=options)
 
-driver.get("https://r.gnavi.co.jp/area/tokyo/izakaya/rs/")
-
-elems = driver.find_elements(By.CLASS_NAME, "style_titleLink__oiHVJ")
-# store_links = [elem.get_attribute("href") for elem in elems]
-store_links = []
-for elem in elems:
-    if "PREF" not in elem.get_attribute('href'): #PRを除外
-            store_links.append(elem.get_attribute('href'))
-print(store_links)
 
 #デジタル証明書確認
 def ssl_check(url):
@@ -50,37 +44,40 @@ def split_address(address):
         return match.groups()
     return "", "", address  # 住所が特殊な場合はそのまま返す  
 
-# for i in range(1,4):
-#     # 1ページ目はそのままのURL
-#     if i==1:
-#         driver.get("https://r.gnavi.co.jp/area/tokyo/izakaya/rs/")
-#     else:
-#     # 2ページ以降はURLをつなげる
-#         page_url=driver.find_element(By.XPATH,'//*[@id="__next"]/div/div[2]/main/div[11]/nav/ul/li[10]/a').get_attribute('href')
-#         driver.get(page_url)
-#         print(page_url)
-    
-#     # 1～3ページのそれぞれのURLにアクセス
-#     elems = driver.find_elements(By.CLASS_NAME, "style_titleLink__oiHVJ")
-#     store_links = []
-#     for elem in elems:
-#         if "PREF" not in elem.get_attribute('href'): #PRを除外
-#                 store_links.append(elem.get_attribute('href'))
-#         time.sleep(3) #アイドリングタイム
 
-# print(store_links)
-# print(len(store_links))
+store_links = []
+for i in range(1,3):
+    # 1ページ目のURL
+    if i==1:
+        driver.get("https://r.gnavi.co.jp/area/tokyo/izakaya/rs/")
+    else:
+    # 2ページ以降のURL
+        #次へボタンの画像の親要素を取得
+        img_element = driver.find_element(By.CLASS_NAME, "style_nextIcon__M_Me_")
+        #親要素のURLを取得
+        page_url = img_element.find_element(By.XPATH, "..").get_attribute('href')
+        # print(page_url)
+        driver.get(page_url)
+    
+    # それぞれのページの店舗ページURLのリストを取得
+    elems = driver.find_elements(By.CLASS_NAME, "style_titleLink__oiHVJ")
+    for elem in elems:
+        if "PREF" not in elem.get_attribute('href'): #PRを除外
+                store_links.append(elem.get_attribute('href'))
+    time.sleep(3) #アイドリングタイム
+    
+
 
 #各店舗情報を取得
 data=[]
-for store_link in store_links[:1]:
+for store_link in store_links[:50]:
     store_res = driver.get(store_link)
     time.sleep(3) #アイドリングタイム
 
     store_name = driver.find_element(By.ID, "info-name").text
     store_phone = driver.find_element(By.CLASS_NAME, "number").text
 
-    #メールがない場合があるため、空文字を入れる
+    #メールがない場合、空文字を入れる
     try:
         store_mail = driver.find_element(By.CSS_SELECTOR, ".mail").text
     except NoSuchElementException:
@@ -88,17 +85,21 @@ for store_link in store_links[:1]:
 
     region = driver.find_element(By.CLASS_NAME, "region").text
     
-    #建物名がない場合があるため、空文字を入れる
+    #建物名がない場合、空文字を入れる
     try:
         locality = driver.find_element(By.CLASS_NAME, "locality").text
     except NoSuchElementException:
         locality = ""
     
     prefecture ,city ,street = split_address(region)
-    # print(store_name, store_phone,prefecture,city,street,locality)    
-    store_url = driver.find_element(By.XPATH, '//*[@id="sv-site"]/li/a').get_attribute("href")
+    
+    #URLがない場合、空文字を入れる
+    try:
+        store_url = driver.find_element(By.XPATH, '//*[@id="sv-site"]/li/a').get_attribute("href")
+    except NoSuchElementException:
+        store_url = ""
     store_ssl = ssl_check(store_url)
-    data.append([store_name, store_phone,  prefecture, city, street, locality, store_url, store_ssl])
+    data.append([store_name, store_phone,  store_mail, prefecture, city, street, locality, store_url, store_ssl])
 print(data)
 
 # CSV出力
